dcd122628d09d60b33ebe77fd4e5aa3e892bd05b
practicespringboot <springboot1111@gmil.com>
2022-09-21 12:33:49 +0530

    initial commit

---
+++ src/test/java/com/example/Practicespringboot/PracticespringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Practicespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticespringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
